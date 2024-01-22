70a064ae102a442218e94a9badc4160ef97d7768
PobedynskyjV <vp@readable.games>
2024-01-22 18:59:49 +0200

    Added blocks created prefabs and made movement platform

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    
+    [SerializeField] private float speed = 100f;
+    private Vector3 _startPosition;
+
+    void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    
+    void Update()
+    {
+        transform.position += Vector3.back * speed * Time.deltaTime;
+    }
+}
+ 
