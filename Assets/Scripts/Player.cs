48d3bf628f0a35c614fcedc105fdf1a47c11ae79
PobedynskyjV <vp@readable.games>
2024-01-30 17:17:39 +0200

    comented

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Transform playerTransform;
     
     [SerializeField] private RoadSpawner roadSpawner;
 
+
+    private void FixedUpdate()
+    {
+        /*if(Input.GetMouseButton(0))
+        {
+            PlayerMove();
+        }*/
+    }
+
+    private void PlayerMove()
+    {
+        /*float halfScreen = Screen.width / 2;
+        float xPos = (Input.mousePosition.x - halfScreen) / halfScreen;
+
+        playerTransform.localPosition = new Vector3(xPos, 1, 0);*/
+    }
+    
+    
     private void OnTriggerEnter(Collider other)
     {
         roadSpawner.Spawn();
